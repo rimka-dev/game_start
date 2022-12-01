584a8d7f9c42cd58371628ea98fea7c0da80e5af
rimka-dev <59959027+rimka-dev@users.noreply.github.com>
2022-12-01 15:03:11 +0100

    Updates

---
+++ precache-manifest.f0b49db81d61cc91941c399c88ceec4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a5427273dcb0aff7ec7",
+    "url": "/startGameReact/static/css/main.0a0cbdfa.chunk.css"
+  },
+  {
+    "revision": "6a5427273dcb0aff7ec7",
+    "url": "/startGameReact/static/js/main.6a542727.chunk.js"
+  },
+  {
+    "revision": "a96734f1aa8b27447a4d",
+    "url": "/startGameReact/static/js/1.a96734f1.chunk.js"
+  },
+  {
+    "revision": "58a403267b500a48acf5",
+    "url": "/startGameReact/static/js/2.58a40326.chunk.js"
+  },
+  {
+    "revision": "a5f5d3d1b6b636c9bc66",
+    "url": "/startGameReact/static/js/runtime~main.a5f5d3d1.js"
+  },
+  {
+    "revision": "25c84cb0b8b6b03412fae240062f9768",
+    "url": "/startGameReact/static/media/imgStart.25c84cb0.png"
+  },
+  {
+    "revision": "cbafc55f9f39a95d0c275c7e3d6fe3b6",
+    "url": "/startGameReact/index.html"
+  }
+];
